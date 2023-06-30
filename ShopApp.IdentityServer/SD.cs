5bf537ae6f546518a62d27262136a57b3c1a168b
Idriss <Idriss@DESKTOP-TJ1RNSR>
2023-06-30 11:24:07 +0100

    COnfigure ApiScopes Ressources

---
+++ ShopApp.IdentityServer/SD.cs
@@ -0,0 +1,54 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer;
+
+namespace ShopApp.IdentityServer
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+    };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>()
+            {
+                new ApiScope("shopapp","ShopApp Server"),
+                new ApiScope(name:"read", displayName: "Read your data."),
+                new ApiScope(name: "write", displayName: "Write your data"),
+                new ApiScope(name:"delete", displayName: "Delete your data")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>()
+            {
+                new Client
+                {
+                   ClientId="client",
+                    ClientSecrets= { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes={ "read", "write","profile"}
+                },
+                 new Client
+                {
+                    ClientId = "shopapp",
+                    ClientSecrets= { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:44357/signin-oidc" },
+                    PostLogoutRedirectUris={ "https://localhost:44357/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                         IdentityServerConstants.StandardScopes.Email,
+                         "shopapp"
+                    }
+                }
+            };
+    }
+}
