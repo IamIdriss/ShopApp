f9b05c4f7d41190b409afd04cc098e31bbfef905
Idriss <Idriss@DESKTOP-TJ1RNSR>
2023-06-30 10:58:04 +0100

    Adding Identity ServerDbContext

---
+++ ShopApp.IdentityServer/IdentityServerData/IdentityServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopApp.IdentityServer.IdentityServerData.Models;
+
+namespace ShopApp.IdentityServer.IdentityServerData
+{
+    public class IdentityServerDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityServerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
