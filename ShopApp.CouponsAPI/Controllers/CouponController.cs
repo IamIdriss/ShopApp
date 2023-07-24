03266ed8a0a6175251ec2210587610a95e86b667
Idriss <Idriss@DESKTOP-TJ1RNSR>
2023-07-24 08:54:09 +0100

    AddingCouponController

--- ShopApp.CouponsAPI/Controllers/CouponController.cs
+++ ShopApp.CouponsAPI/Controllers/CouponController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShopApp.CouponsAPI.Models.Dtos;
+using ShopApp.CouponsAPI.Repository;
 
 namespace ShopApp.CouponsAPI.Controllers
 {
@@ -7,5 +9,32 @@ namespace ShopApp.CouponsAPI.Controllers
     [ApiController]
     public class CouponController : ControllerBase
     {
+        private readonly ICouponRepository _couponRepository;
+        private readonly CouponResponseDto _response;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            _response = new CouponResponseDto();
+        }
+
+        [HttpGet("{code}")]
+        public async Task<CouponResponseDto> GetCoupon(string code)
+        {
+            try
+            {
+                var couponDto = await _couponRepository.GetCouponByCode(code);
+                _response.Result = couponDto;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>
+                {
+                    e.ToString()
+                };
+            }
+            return _response;
+        }
     }
 }
