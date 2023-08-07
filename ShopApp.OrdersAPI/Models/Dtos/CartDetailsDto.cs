23c2afd89d36915be4905eb0de921a1dac578798
Idriss <Idriss@DESKTOP-TJ1RNSR>
2023-08-07 15:39:08 +0100

    Adding Dtos for OrdersAPI

---
+++ ShopApp.OrdersAPI/Models/Dtos/CartDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopApp.OrdersAPI.Models.Dto
+{
+    public class CartDetailsDto
+    {
+
+
+        public int CartDetailId { get; set; }
+
+        public int CartHeaderId { get; set; }
+        
+
+        public int ProductId { get; set; }
+        public virtual ProductsDto Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ ShopApp.OrdersAPI/Models/Dtos/CheckoutMessageDto.cs
@@ -0,0 +1,23 @@
+﻿using ShopApp.OrdersAPI.Models.Dto;
+
+namespace ShopApp.OrdersAPI.Models.Dtos
+{
+    public class CheckoutMessageDto
+    {
+        public int CartHeaderId { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; } = "";
+        public double OrderTotal { get; set; }
+        public double DiscountTotal { get; set; } = 0;
+        public double GrandTotal { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpiryMonthYear { get; set; }
+        public IEnumerable<CartDetailsDto> CartDetails { get; set; }
+    }
+}
