6d4b1e6ea0d86568d97c2b2689702d490a425f11
Idriss <Idriss@DESKTOP-U23R614>
2023-08-16 15:21:29 +0100

    Coupon Validation Via Shopping Cart API

---
+++ ShopApp.ShoppingCartAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShopApp.ShoppingCartAPI.Models.Dtos;
+
+namespace ShopApp.ShoppingCartAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCode(string couponCode);
+    }
+}
