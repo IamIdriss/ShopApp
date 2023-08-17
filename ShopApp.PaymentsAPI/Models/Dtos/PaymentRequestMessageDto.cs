e2d8ab7532c81bc3463d130f40a8271576bfd221
Idriss <Idriss@DESKTOP-U23R614>
2023-08-17 11:59:23 +0100

    Add Payment Message

---
+++ ShopApp.PaymentsAPI/Models/Dtos/PaymentRequestMessageDto.cs
@@ -0,0 +1,15 @@
+﻿using ShopApp.MessageBus;
+
+namespace ShopApp.PaymentsAPI.Models.Dtos
+{
+    public class PaymentRequestMessageDto : BaseMessage
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpiryMonthYear { get; set; }
+        public double OrderTotal { get; set; }
+    }
+}
---
+++ ShopApp.PaymentsAPI/Models/Dtos/PaymentUpdateMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopApp.PaymentsAPI.Models.Dtos
+{
+    public class PaymentUpdateMessageDto
+    {
+        public int OrderId { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+    }
+}
