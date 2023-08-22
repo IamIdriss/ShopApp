929b8b7dc3c5499d9410ce658e5f958145f16d47
Idriss <Idriss@DESKTOP-U23R614>
2023-08-22 12:56:23 +0100

    Publish Message to Exchange

--- ShopApp.PaymentsAPI/Program.cs
+++ ShopApp.PaymentsAPI/Program.cs
@@ -1,10 +1,14 @@
+using ShopApp.PaymentsAPI.RabbitMQSender;
 using ShopApp.PaymentsAPI.Repository;
+using ShopApp.PaymentsAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddSingleton<IPaymentProcessor, PaymentProcessor>();
+builder.Services.AddSingleton<IRabbitMQPaymentResultMessageSender, RabbitMqPaymentResultMessageSender>();
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
