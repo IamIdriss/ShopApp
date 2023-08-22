79039fb126d9609d8e25d016b77e38196d0e5e9f
Idriss <Idriss@DESKTOP-U23R614>
2023-08-22 13:14:53 +0100

    Configure  Exchange Subscriptions

---
+++ ShopApp.EmailsAPI/Services/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,73 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using ShopApp.EmailsAPI.Repository;
+using System.Text;
+using Newtonsoft.Json;
+using ShopApp.EmailsAPI.Models.Dtos;
+
+namespace ShopApp.EmailsAPI.Services
+{
+    public class RabbitMQPaymentConsumer :BackgroundService
+    {
+        private readonly string _hostname;
+        private readonly string _username;
+        private readonly string _password;
+        private readonly EmailRepository _emailRepository;
+        private IConnection _connection;
+        private IModel _channel;
+        //private const string _fanoutExchangeName = "Fanout_Payment_Update_Exchange";
+        private const string _directExchangeName = "Direct_Payment_Update_Exchange";
+        private const string _paymentEmailQueueName = "Payment_Email_Queue";
+        string queueName = "";
+
+        public RabbitMQPaymentConsumer(EmailRepository emailRepository)
+        {
+            _hostname = "localhost";
+            _username = "guest";
+            _password = "guest";
+            _emailRepository = emailRepository;
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostname,
+                UserName = _username,
+                Password = _password
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            //_channel.ExchangeDeclare(_fanoutExchangeName, ExchangeType.Fanout);
+            //queueName = _channel.QueueDeclare().QueueName;
+            _channel.ExchangeDeclare(_directExchangeName, ExchangeType.Direct, false);
+            _channel.QueueDeclare(_paymentEmailQueueName, false, false, false);
+            //_channel.QueueBind(queueName, _fanoutExchangeName, "");
+            _channel.QueueBind(_paymentEmailQueueName, _directExchangeName, "PaymentEmail");
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                var paymentUpdateMessageDto = JsonConvert.DeserializeObject<PaymentUpdateMessageDto>(content);
+                HandleMessage(paymentUpdateMessageDto).GetAwaiter().GetResult();
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+            //_channel.BasicConsume(queueName, false, consumer);
+            _channel.BasicConsume(_paymentEmailQueueName, false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessage(PaymentUpdateMessageDto paymentUpdateMessageDto)
+        {
+
+            try
+            {
+                await _emailRepository.SendAndLogEmail(paymentUpdateMessageDto);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
